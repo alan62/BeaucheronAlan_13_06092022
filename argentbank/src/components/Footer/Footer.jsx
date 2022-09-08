78ff29d7e4c8fec39b616ef516399c4905f22129
Alan62 <beaucheron.alan@free.fr>
2022-09-08 13:12:34 +0200

    création du component footer

---
+++ argentbank/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./footer.css"
+/**
+ * Footer of Argent Bank
+ * @returns {ReactElement}
+ */
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p className="footer__text">Copyright 2020 Argent Bank</p>
+    </footer>
+  )
+}
+
+export default Footer
