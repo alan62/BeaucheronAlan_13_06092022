4b5185a7f4a8be7b2dee2c3acfebab65c951e4ca
Alan62 <beaucheron.alan@free.fr>
2022-09-08 13:11:54 +0200

    nettoyage fichier app.js index.js/css

---
+++ argentbank/src/App.js
@@ -0,0 +1,16 @@
+import Router from "./Routes/Router"
+
+
+/**
+ *App
+ * @returns {ReactElement}
+ */
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  )
+}
+
+export default App
