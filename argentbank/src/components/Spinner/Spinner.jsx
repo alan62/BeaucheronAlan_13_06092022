b99f1766526766480190e1862a71899bce5f81f1
Alan62 <beaucheron.alan@free.fr>
2022-09-09 15:47:55 +0200

    création du spinner

---
+++ argentbank/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import "./spinner.css"
+/**
+ * Spinner
+ * @returns {ReactElement}
+ */
+const Spinner = () => {
+  return (
+    <section className="spinnerContainer">
+      <span className="firstLetter">L</span>
+      <span className="secondLetter">o</span>
+      <span className="thirdLetter">a</span>
+      <span className="fourthLetter">d</span>
+      <span className="fifthLetter">i</span>
+      <span className="sixthLetter">n</span>
+      <span className="seventhLetter">g</span>
+    </section>
+  )
+}
+
+export default Spinner
