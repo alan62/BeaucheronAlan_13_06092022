0a917deef3499e2a10d13330b4e4b480c4b7e769
Alan62 <beaucheron.alan@free.fr>
2022-12-19 10:13:53 +0100

    page signIn + userSlice

--- argentbank/src/pages/SignIn/SignIn.jsx
+++ argentbank/src/pages/SignIn/SignIn.jsx
@@ -1,21 +1,51 @@
-import React from "react"
-import "./signIn.css"
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchToken } from "../../features/auth";
+import "./signIn.css";
 
 const SignIn = () => {
-  
-  document.title = "Argent Bank - Login Page"
+  const [username, setUsername] = useState(" ");
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+
+  let localStorage = window.localStorage.user
+    ? JSON.parse(window.localStorage.user)
+    : { username, password };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let value = { username, password, checked };
+    dispatch(fetchToken({ value }));
+    navigate("/profile");
+  };
+
+  const handleCheckbox = (e) => {
+    setChecked(e.target.checked);
+  };
+
+  useEffect(() => {
+    setUsername(localStorage.username);
+    setPassword(localStorage.password);
+  }, [localStorage.username, localStorage.password]);
+  document.title = "Argent Bank - Login Page";
 
   return (
     <div className="main">
       <div className="signInContainer">
         <i className="fa fa-user-circle signIn__icon"></i>
         <h1 className="signIn__h1">Sign In</h1>
-        <form className="signIn__form">
+        <form onSubmit={handleSubmit}>
           <div className="signIn__input">
             <label htmlFor="username">Username</label>
             <input
               type="text"
               id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value.trim())}
             />
           </div>
           <div className="signIn__input">
@@ -23,18 +53,19 @@ const SignIn = () => {
             <input
               type="password"
               id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value.trim())}
             />
           </div>
           <div className="signIn__checkbox">
-            <input  type="checkbox" id="checkbox" />
+            <input onChange={handleCheckbox} type="checkbox" id="checkbox" />
             <label htmlFor="checkbox">Remember me</label>
           </div>
-
           <input className="signInBtn" type="submit" value="Sign In" />
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- argentbank/src/slices/userSlice.js
+++ argentbank/src/slices/userSlice.js
@@ -1,58 +1,76 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { fetchUserDatas, updateUserDatas } from "../features/user"
+import {
+  createSlice
+} from "@reduxjs/toolkit"
+
+import {
+  fetchUserDatas,
+  updateUserDatas
+} from "../features/user"
 
 /**
  * slice user of redux store
  * @return {Object} redux store modified
  */
- const userSlice = createSlice({
-    name: "user",
-    initialState: {
-      firstName: "",
-      lastName: "",
-      editName: false,
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    firstName: "",
+    lastName: "",
+    editName: false,
+  },
+  reducers: {
+    editName: (state) => {
+      state.editName = true
+      return state
     },
-    reducers: {
-      editName: (state) => {
-        state.editName = true
-        return state
-      },
-      resetUser: (state) => {
-        state = { firstName: "", lastName: "", editName: false }
-        return state
-      },
-      abort: (state) => {
-        state.editName = false
-        return state
-      },
+    resetUser: (state) => {
+      state = {
+        firstName: "",
+        lastName: "",
+        editName: false
+      }
+      return state
     },
-    extraReducers: {
-      [fetchUserDatas.pending]: () => {
-        console.log("Pending")
-      },
-      [fetchUserDatas.fulfilled]: (state, { payload }) => {
-        console.log("Fetch Successfully !")
-        return {
-          firstName: payload.body.firstName,
-          lastName: payload.body.lastName,
-          id: payload.body.id,
-          createdAt: payload.body.createdAt,
-          updatedAt: payload.body.updatedAt,
-          status: payload.status,
-          editName: false,
-        }
-      },
-      [fetchUserDatas.rejected]: () => {
-        console.log("Rejected !")
-      },
-      [updateUserDatas.fulfilled]: (state, { payload }) => {
-        return {
-          ...state,
-          status: payload,
-        }
-      },
+    abort: (state) => {
+      state.editName = false
+      return state
     },
-  })
-  
-  export const { editName, resetUser, abort } = userSlice.actions
-  export default userSlice.reducer
+  },
+  extraReducers: {
+    [fetchUserDatas.pending]: () => {
+      console.log("Pending")
+    },
+    [fetchUserDatas.fulfilled]: (state, {
+      payload
+    }) => {
+      console.log("Fetch Successfully !")
+      return {
+        firstName: payload.body.firstName,
+        lastName: payload.body.lastName,
+        id: payload.body.id,
+        createdAt: payload.body.createdAt,
+        updatedAt: payload.body.updatedAt,
+        status: payload.status,
+        editName: false,
+      }
+    },
+    [fetchUserDatas.rejected]: () => {
+      console.log("Rejected !")
+    },
+    [updateUserDatas.fulfilled]: (state, {
+      payload
+    }) => {
+      return {
+        ...state,
+        status: payload,
+      }
+    },
+  },
+})
+
+export const {
+  editName,
+  resetUser,
+  abort
+} = userSlice.actions
+export default userSlice.reducer
